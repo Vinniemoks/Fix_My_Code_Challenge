a4deab5baeaf5e81af8e9796144a8b13e28c0f5a
Vincent Mokua <mokua.vinny@gmail.com>
2025-03-24 08:53:14 -0700

    init

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,22 +6,19 @@
     of the program.
 */
 
+
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.exit(1)
 }
 
-let size = parseInt(process.argv[2], 10);  // Use base 10
+size = parseInt(process.argv[2], 16)
 
-// Check if size is a valid number and greater than 0
-if (isNaN(size) || size <= 0) {
-    process.stderr.write("Size must be a positive number\n");
-    process.exit(1);
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
 }
-
-for (let i = 0; i < size; i++) {
-    console.log("#".repeat(size));  // Simplified loop
-}
-
